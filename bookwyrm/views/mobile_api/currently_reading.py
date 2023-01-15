1a175122088bb56d7913d859a72563fe24777431
Dustin Steiner <dustin.steiner@gmail.com>
2023-01-15 22:21:21 +0000

    chore: more elegant currently reading api

--- bookwyrm/views/mobile_api/currently_reading.py
+++ bookwyrm/views/mobile_api/currently_reading.py
@@ -1,36 +1,32 @@
 """ currently reading """
 from django.http import JsonResponse
-from django.contrib.auth.decorators import login_required
 
 from bookwyrm.oauth import provider
 from bookwyrm.views.feed import get_suggested_books
 from bookwyrm.settings import DOMAIN, MEDIA_URL
 
+def copy_attrs(origin, attrs):
+	result = {}
+	for attr in attrs:
+		result[attr] = getattr(origin, attr)
+	return result
+
 def map_readthrough(readthrough):
-	return {
-		'id': readthrough.id,
-		'progress': readthrough.progress or 0,
-		'mode': readthrough.progress_mode,
-	} if readthrough else None
+	return copy_attrs(readthrough, ['id', 'progress', 'progress_mode']) if readthrough else None
 
 def map_author(author):
-	return {
-		'id': author.id,
-		'name': author.name,
-	}
+	return copy_attrs(author, ['id', 'name'])
 
 def map_book(book):
-	return {
-		'id': book.id,
-		'title': book.title,
-		'authors': list(map(map_author, book.authors.all())),
-		'cover': f"https://{DOMAIN}{MEDIA_URL}{book.cover}" if book.cover else None,
-		'latest_readthrough': map_readthrough(book.latest_readthrough),
-	}
+	result = copy_attrs(book, ['id', 'title'])
+	result['authors'] = list(map(map_author, book.authors.all()))
+	result['cover'] = f"https://{DOMAIN}{MEDIA_URL}{book.cover}" if book.cover else None
+	result['latest_readthrough'] = map_readthrough(book.latest_readthrough)
+	return result
 
 @provider.protected_resource_view(scopes=['user'])
 def currently_reading(request):
 	suggested_books = get_suggested_books(request.user)
 	reading_shelf = next(filter(lambda shelf: shelf['identifier'] == 'reading', suggested_books))
-	books = list(map(map_book, reading_shelf['books']))
-	return JsonResponse(books, safe=False)
+	data = list(map(map_book, reading_shelf['books']))
+	return JsonResponse(data, safe=False)
