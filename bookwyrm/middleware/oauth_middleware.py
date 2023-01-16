fc4852d244f56a2d1da7e696c06c7fcd59794b09
Dustin Steiner <dustin.steiner@gmail.com>
2023-01-16 20:02:21 +0000

    fix: catch oauth middleware exceptions

--- bookwyrm/middleware/oauth_middleware.py
+++ bookwyrm/middleware/oauth_middleware.py
@@ -9,12 +9,15 @@ class OAuthAuthenticationMiddleware:
 		self._resource_endpoint = server
 	
 	def __call__(self, request):
-		if not request.path.startswith('/oauth') and not request.user.is_authenticated:
-			uri, http_method, body, headers = extract_params(request)
+		try:
+			if not request.path.startswith('/oauth') and not request.user.is_authenticated:
+				uri, http_method, body, headers = extract_params(request)
 
-			valid, r = self._resource_endpoint.verify_request(uri, http_method, body, headers, None)
-			if valid:
-				request.user = r.user
-				#request.client = r.client
+				valid, r = self._resource_endpoint.verify_request(uri, http_method, body, headers, None)
+				if valid:
+					request.user = r.user
+					#request.client = r.client
+		except:
+			pass
 		
 		return self.get_response(request)
